8f4c6f3cb46f6ab4a311daee80cd7e825e8cd827
Vinícius Baião Pires <85589369+Baiaopires@users.noreply.github.com>
2023-12-19 12:08:56 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -233,8 +233,8 @@ def plot_graph():
         expr = sympify(expression)
         
         # Determine a dimensão da expressão
-        dimension = expr.free_symbols
-        dimension_count = len(dimension)
+        if "y" in expression: dimension_count = 2
+        else: dimension_count = 1
         
         #Definição da fonte do título
         csfont = {'fontname':'Arial'}
@@ -268,8 +268,8 @@ def plot_graph():
             fig = plt.figure(facecolor='#303030')
             ax = fig.add_subplot(111, projection='3d', facecolor="#303030")
             
-            x_vals = np.linspace(-10, 10, 100)
-            y_vals = np.linspace(-10, 10, 100)
+            x_vals = np.linspace(-1000, 1000, 100)
+            y_vals = np.linspace(-1000, 1000, 100)
             X, Y = np.meshgrid(x_vals, y_vals)
             
             Z = np.vectorize(lambda v1, v2: expr.subs({x: v1, y: v2}))(X, Y)
